af17893433f383cd4a58aa07e47e6db0ea661786
slopero <68631093+slopero@users.noreply.github.com>
2025-02-06 19:20:18 +0700

    Delete Pages directory

--- Pages/Cataloge/Cards/Page1.cshtml.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Mvc.RazorPages;
-
-public class Page1Model : PageModel
-{
-    public void OnGet()
-    {
-        // Вы можете добавить сюда свой код для обработки логики страницы, если нужно.
-    }
-}
